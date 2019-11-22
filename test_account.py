7605ce13eee2f7ab37257414babf37e6723af8f1
Michał Klich <michal@klich.dev>
2019-11-22 22:24:07 +0100

    Add account tests

---
+++ test_account.py
@@ -0,0 +1,47 @@
+import uuid
+
+import pytest
+
+from account import Account
+
+
+def test_account_deposit():
+    acc = Account(uuid.uuid4())
+
+    acc.deposit(100)
+
+    assert acc.balance == 100
+
+
+def test_account_withdrawal_enough_balance():
+    acc = Account(uuid.uuid4(), balance=100)
+
+    acc.withdrawal(100)
+
+    assert acc.balance == 0
+
+def test_account_withdrawal_short_balance():
+    acc = Account(uuid.uuid4())
+
+    with pytest.raises(BalanceTooLow):
+        acc.withdrawal(100)
+
+
+def test_account_history_log():
+    acc_no = uuid.uuid4()
+    acc = Account(acc_no), balance=100)
+    acc.withdrawal(100)
+    with pytest.raises(BalanceTooLow):
+        acc.withdrawal(100)
+    acc.deposit(100)
+    cash = acc.close()
+
+    assert cash == 100
+    assert acc.history == sorted(acc.history)  # is it sorted?
+    assert acc.history == [
+        (1573729201.0, f"Account {acc_no} opened with balance 100"),
+        (1573729202.0, "Withdrawal 100"),
+        (1573729203.0, "Withdrawal 100 failed. BalanceTooLow"),
+        (1573729204.0, "Deposit 100"),
+        (1573729205.0, f"Account {acc_no} closed. Returned 100 cash")
+    ]
